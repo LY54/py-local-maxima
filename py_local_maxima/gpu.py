201cd2f7551597c7192bd1e5e6eb766aa54876be
Ben Hollar <benh524@gmail.com>
2020-12-03 17:14:05 -0500

    Now locating peaks on GPU instead of CPU

--- py_local_maxima/gpu.py
+++ py_local_maxima/gpu.py
@@ -31,13 +31,41 @@ __global__ void NaiveDilationKernel(float *src, float *dst, int width, int heigh
 """)
 naive_dilation = naive_dilation_mod.get_function("NaiveDilationKernel")
 
+eq_and_thresh_mod = SourceModule("""
+__global__ void EqualsAndThresholdKernel(float *a, float *b, float threshold, int width, int height, bool *dst) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (y >= height || x >= width) {
+        return;
+    }
+    int flatIndex = y * width + x;
+    dst[flatIndex] = (a[flatIndex] == b[flatIndex]) && (threshold < a[flatIndex]);
+}
+""")
+eq_and_thresh = eq_and_thresh_mod.get_function("EqualsAndThresholdKernel")
+
 
 def detect_naive(image, neighborhood, threshold=1e-12):
-    """TODO
+    """Detect peaks using naive image dilation kernel
 
-    NOTE: `neighborhood` should become an integer, not a boolean matrix as in
-          cpu code. It specifies the side length of a square window and is
-          expected to be odd
+    Parameters
+    ----------
+    image : numpy.ndarray (2D)
+        The imagery to find the local maxima of. Note this will be reduced
+        to 32-bit float precision due to GPU constraints.
+    neighborhood : numpy.ndarray (2D)
+        A boolean matrix specifying a scanning window for maxima detection.
+        The neigborhood size is implicitly defined by the matrix dimensions.
+        Note that the values of this matrix are currently ignored, but
+        conceivably could be in the future for more complex windowing.
+    threshold : float
+        The minimum acceptable value of a peak
+
+    Returns
+    -------
+    numpy.ndarray (2D)
+        A boolean matrix specifying maxima locations (True) and background
+        locations (False)
     """
 
     # Image must be 32-bit for GPU code to work at all. Force cast our input
@@ -60,7 +88,14 @@ def detect_naive(image, neighborhood, threshold=1e-12):
                    block=block,
                    grid=grid)
 
-    # TODO: Perform peak-finding on GPU, not CPU
-    detected_peaks = gpu_dilated_image.get() == image
-    detected_peaks[image < threshold] = False
-    return detected_peaks
+    # Detect the peaks (locations where original image equals dilated)
+    gpu_detected_peaks = gpuarray.empty_like(gpu_image, dtype=numpy.bool)
+    eq_and_thresh(gpu_image.gpudata,
+                  gpu_dilated_image.gpudata,
+                  numpy.float32(threshold),
+                  numpy.int32(image.shape[0]),
+                  numpy.int32(image.shape[1]),
+                  gpu_detected_peaks.gpudata,
+                  block=block,
+                  grid=grid)
+    return gpu_detected_peaks.get()
